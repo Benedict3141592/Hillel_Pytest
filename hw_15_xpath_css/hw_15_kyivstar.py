974998fda6efdf1ecb44517676b381da51b030f3
Rodion Groshev <rodion.groshev@gmail.com>
2023-07-09 20:38:33 +0200

    hw_15

---
+++ hw_15_xpath_css/hw_15_kyivstar.py
@@ -0,0 +1,43 @@
+# //body/div/header/nav/a/span[text()='Приватним особам']          https://kyivstar.ua/
+# //*[@alt="kyivstar-logo-full"]
+# //span[@class="Profile_profile__text__CP40o"]
+# //*[@id="chipsBlock"]/div/div[1]/a
+# //*[@id="main-header"]/nav/a/span[text()="Бізнесу"]
+# //*[text()="Бізнесу"]
+# //body/div/header/nav/a[4]
+# //*[@id="__next"]/footer/div/section/child::div[1]
+# //input[@type="tel"]
+# //a[@id]
+# //button[text()]
+# //button//span[contains(text(),"Київстар")]
+# //child::ul/child::li/child::a/img
+# //*/span[text()="Роумінг"]
+# //ul[child::li][starts-with(@class, "Main")]
+# //*[@id='esim']
+# //body/child::div/header/div/div/span
+# //*[@target][@href="/pay"]
+# //*[starts-with(@class, "btn")][@href="/pay"]
+# //*[@href="/tariffs"][starts-with(@class, "btn")]
+# //button//span[contains(text(),"Мобільний")]
+# //ul[*]/li/a[@href="/promotions"]
+# //*[@id="tariffBlock"]//div[starts-with(@class, "Arrow_arrow-next")]/button
+# //p[text()="Смартфони"]
+# //a[@aria-label="NO_DATA"][1]
+# //a[@rel="noopener noreferrer"][2]
+# //a//div/div/img[@alt="money icon"]
+# //*[@href="tel:460"]
+# //*[@href="https://t.me/Ivanka_Kyivstar"]
+# //div/a[@href="https://www.facebook.com/kyivstar"]
+#
+# CSS
+#
+# #30 a[href^="https://www.facebook"]
+# #28 [href="tel:460"]
+# #26 a[href*="mk3_android"]
+# #24 div.slick-slide.slick-active.slick-current > div > a > div > p
+# #22 [href="/promotions"]
+# #20 div[class*="wgyMB"] > a
+# #18 a[class*="nkw"][href="/pay"]
+# #16 #esim
+# #14 div[class="accordion__content"] > a:nth-child(6) > div > span
+# #12 button[data-analytics*="3"]
